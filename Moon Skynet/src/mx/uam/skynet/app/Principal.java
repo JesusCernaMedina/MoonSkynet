7f76a89add62857817e5ff5348bfdad772b19c74
jcm.programing@gmail.com <Jesus@JesusCerna>
2018-03-12 08:58:44 -0600

    Agregando un comentario Prueba Commit

--- Moon Skynet/src/mx/uam/skynet/app/Principal.java
+++ Moon Skynet/src/mx/uam/skynet/app/Principal.java
@@ -6,7 +6,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		//Esta es una nueva modificacion
 		Inicio ini = new Inicio();
 		ini.setVisible(true);
 
