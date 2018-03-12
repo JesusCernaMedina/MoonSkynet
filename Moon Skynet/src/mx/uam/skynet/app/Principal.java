4cfb2709169251d7fbeec909c9c7100a7fd9534b
jcm.programing@gmail.com <Jesus@10.2.76.199>
2018-03-12 13:35:52 -0600

    Corrigiendo Errores

--- Moon Skynet/src/mx/uam/skynet/app/Principal.java
+++ Moon Skynet/src/mx/uam/skynet/app/Principal.java
@@ -6,10 +6,10 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//Esta es una nueva modificacion
+
 		Inicio ini = new Inicio();
 		ini.setVisible(true);
 
 	}
-
+	
 }
