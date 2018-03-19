4522319ff859dc76b892cf73df9f43490ffced96
Polet Kanddlove <Polet Kanddlove@192.168.1.70>
2018-03-18 20:15:25 -0600

    Avances

--- Moon Skynet/src/mx/uam/skynet/app/persistencia/ConnectDB.java
+++ Moon Skynet/src/mx/uam/skynet/app/persistencia/ConnectDB.java
@@ -9,21 +9,63 @@ import java.sql.Statement;
 import java.sql.Connection;
 
 /**
-* @author Jesus Cerna Medina
-*/
+ * @author Jesus Cerna Medina
+ */
 public class ConnectDB {
-	
+
 	//////////////////inicio Datos Cliente ////////////////////////@author Gabriel Lopez Hernandez
-	
+
 	private static String apellido;
 	private static String nombre;
 	private static String telefono;
 	private static String correo;
 	private static int fh_nacimento;
 	private static String direccion;
+	private static int folio_cita;
+	private int row;
 	
 	
 	
+	public static int getFolio_cita() {
+		return folio_cita;
+	}
+
+	public static void setFolio_cita(int folio_cita) {
+		ConnectDB.folio_cita = folio_cita;
+	}
+
+	public static String getCita_previa() {
+		return cita_previa;
+	}
+
+	public static void setCita_previa(String cita_previa) {
+		ConnectDB.cita_previa = cita_previa;
+	}
+
+	public static String getCita_nueva() {
+		return cita_nueva;
+	}
+
+	public static void setCita_nueva(String cita_nueva) {
+		ConnectDB.cita_nueva = cita_nueva;
+	}
+
+	private static String cita_previa;
+	private static String cita_nueva; 
+	
+//	ARRAY TYPE OBJECT TO SAVE THE ALL APPOINTMENT OF THE PATIENT
+	private static Object[] obj  ;
+
+
+
+	public static Object[] getObj() {
+		return obj;
+	}
+
+	public static void setObj(Object[] obj) {
+		ConnectDB.obj = obj;
+	}
+
 	public static String getDireccion() {
 		return direccion;
 	}
@@ -71,100 +113,187 @@ public class ConnectDB {
 	public static void setFh_nacimento(int fh_nacimento) {
 		ConnectDB.fh_nacimento = fh_nacimento;
 	}
-	
-//////////////////Fin de Datos Cliente ////////////////////////@author Gabriel Lopez Hernandez
-	
-	
-//////////////////inicio Busqueda cliente especifico ////////////////////////@author Gabriel Lopez Hernandez
+
+	//////////////////Fin de Datos Cliente ////////////////////////@author Gabriel Lopez Hernandez
+
+	//////////////////inicio Busqueda cliente especifico ////////////////////////@author Gabriel Lopez Hernandez
 	public void buscaClienteEspecifico(String folio) throws SQLException {
-		System.out.println("Hola1");
+	
+
 		ResultSet resultApellido = consult("SELECT apellidos FROM pacientes WHERE fol_paciente = '"+folio + "'").executeQuery();
 		if (resultApellido.next()) {
 			apellido = resultApellido.getString(1);
-	        Desconectar();
+			
+			
+			Desconectar();
 		} else {
-			System.out.println("No se encontro nada. 1");
-	        Desconectar();
+//			System.out.println("No se encontro nada. 1");
+			Desconectar();
 		}
-		    
-		    ResultSet resultNombre = consult("SELECT nombre FROM pacientes WHERE fol_paciente = '"+folio+ "'").executeQuery();
-		    if (resultNombre.next()) {
-		    	nombre = resultNombre.getString(1);
-			    Desconectar();
-			} else {
-				System.out.println("No se encontro nada. 2");
-			    Desconectar();
-			}
-		    
-		    
-		    ResultSet resultTelefono = consult("SELECT telefono FROM pacientes WHERE fol_paciente = '"+folio+ "'").executeQuery();
-		    if (resultTelefono.next()) {
-		    	telefono = resultTelefono.getString(1);
-			    Desconectar();
-			} else {
-				System.out.println("No se encontro nada. 3");
-			    Desconectar();
-			}
-		    
-		    
-		    ResultSet resultCorreo = consult("SELECT correo FROM pacientes WHERE fol_paciente = '"+folio+ "'").executeQuery();
-		    if (resultCorreo.next()) {
-		    	correo =  resultCorreo.getString(1);
-			    Desconectar();
-			} else {
-				System.out.println("No se encontro nada. 4");
-			    Desconectar();
-			}
-		    
-		    
-		    ResultSet resultFH_nacimiento = consult("SELECT fh_nacimiento FROM pacientes WHERE fol_paciente = '"+folio+ "'").executeQuery();
-		    if (resultFH_nacimiento.next()) {
-		    	fh_nacimento = resultFH_nacimiento.getInt(1);
-			    Desconectar();
-			} else {
-				System.out.println("No se encontro nada. 5");
-			    Desconectar();
-			}
-         
+
+		ResultSet resultNombre = consult("SELECT nombre FROM pacientes WHERE fol_paciente = '"+folio+ "'").executeQuery();
+		if (resultNombre.next()) {
+			nombre = resultNombre.getString(1);
+			Desconectar();
+		} else {
+//			System.out.println("No se encontro nada. 2");
+			Desconectar();
+		}
+
+
+		ResultSet resultTelefono = consult("SELECT telefono FROM pacientes WHERE fol_paciente = '"+folio+ "'").executeQuery();
+		if (resultTelefono.next()) {
+			telefono = resultTelefono.getString(1);
+			Desconectar();
+		} else {
+//			System.out.println("No se encontro nada. 3");
+			Desconectar();
+		}
+
+		ResultSet resultCorreo = consult("SELECT correo FROM pacientes WHERE fol_paciente = '"+folio+ "'").executeQuery();
+		if (resultCorreo.next()) {
+			correo =  resultCorreo.getString(1);
+			Desconectar();
+		} else {
+			
+			Desconectar();
+		}
+
+
+		ResultSet resultFH_nacimiento = consult("SELECT fh_nacimiento FROM pacientes WHERE fol_paciente = '"+folio+ "'").executeQuery();
+		if (resultFH_nacimiento.next()) {
+			fh_nacimento = resultFH_nacimiento.getInt(1);
+			Desconectar();
+		} else {
+			
+			Desconectar();
+		}
+		
+
+		ResultSet resultDireccion = consult("SELECT direccion FROM pacientes WHERE fol_paciente = '"+folio+ "'").executeQuery();
+		if (resultDireccion.next()) {
+			direccion = resultDireccion.getString(1);
+			Desconectar();
+		} else {
+			
+			Desconectar();
+		}
+		
+
 	}
-//////////////////Fin busqueda cliente especifico ////////////////////////@author Gabriel Lopez Hernandez
+	//////////////////Fin busqueda cliente especifico ////////////////////////@author Gabriel Lopez Hernandez
+
+
+//	SEARCH APOINTMENT PATIENT
+	public ResultSet AppointmentPatient(String folio){
+		Connection cn= Conectar();
+		Statement st;
+		ResultSet rs = null;
+		try{
+			
+			st = cn.createStatement();
+			rs= st.executeQuery("SELECT cita_fol_paciente,fh_ult_cita, descripcion, tratamiento, fh_prox_cita FROM pacientes, citas WHERE fol_paciente= '"+folio+"'" );
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+		}
+		return rs;
+	}
 	
+//	POSTPONE APPOINTMENT
+
+	public void postponeButton(String folio, int folio_cita, String fecha_nueva){
+		Connection cn= Conectar();
+		PreparedStatement pst;
+		
+		try {
+			System.out.println( fecha_nueva+" "+folio +" "+folio_cita );
+			pst= cn.prepareStatement("UPDATE citas, pacientes SET  fh_prox_cita= '"+fecha_nueva+"' WHERE fol_paciente='"+folio+"' AND cita_fol_paciente='"+folio_cita+"'" );
+			row= pst.executeUpdate();
+			System.out.println(row);
+			
+			if (row>0) {
+				System.out.println("Se ha actualizado correctamente");
+				
+				Desconectar();
+			} else {
+				
+				Desconectar();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	// CANCEL APPOINTMENT 
+	public void cancelButton(String folio, int folio_cita, String fecha_nueva){
+		
+		Connection cn= Conectar();
+		PreparedStatement pst;
+		
+		try {
+			System.out.println( fecha_nueva+" "+folio +" "+folio_cita );
+			pst= cn.prepareStatement("delete citas from citas,pacientes where fol_paciente='"+folio+
+					"' and cita_fol_paciente='"+folio_cita+"'");
+			row= pst.executeUpdate();
+			System.out.println(row);
+			
+			if (row>0) {
+				System.out.println("Se ha actualizado correctamente");
+				
+				Desconectar();
+			} else {
+				
+				Desconectar();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
 	public static PreparedStatement consult(String sql) throws SQLException {
 		PreparedStatement consultar = ConnectDB.Conectar().prepareStatement(sql);
 		return consultar;
 	}
-	
 
 	private static Connection expediente;
 	public static  Statement stm;
-	
-    public static Connection Conectar() {
-        try {
-            expediente = DriverManager.getConnection(
-            "jdbc:mysql://localhost:3306/moonskynet"
-             ,"id3287223_artperform","098mklas");
-            
-//            stm = expediente.createStatement();
 
-            if(expediente!=null){
-                System.out.println("Conecxion lista..");
-                
-            }else if(expediente==null){
-                throw new SQLException();
-            }
-		    
-        } catch(Exception error) {
-            JOptionPane.showMessageDialog(null, "Hubo un error en la conexion"+" a la base de datos"+error);
-        }
-        return expediente;
-        
-        
-    }
-    
-    
-    public static Connection Desconectar(){
-        expediente=null;
-        return expediente;
-    }
+	public static Connection Conectar() {
+		try {
+			expediente = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/moonskynet"
+					,"root","");
+
+			if(expediente!=null){
+
+
+			}else if(expediente==null){
+				throw new SQLException();
+			}
+
+		} catch(Exception error) {
+			JOptionPane.showMessageDialog(null, "Hubo un error en la conexion"+" a la base de datos"+error);
+		}
+		return expediente;
+	}
+
+	public static Connection Desconectar(){
+		expediente=null;
+		return expediente;
+	}
 
 }
--- Moon Skynet/src/mx/uam/skynet/app/presentacion/VentanaBuscar.java
+++ Moon Skynet/src/mx/uam/skynet/app/presentacion/VentanaBuscar.java
@@ -1,7 +1,9 @@
 package mx.uam.skynet.app.presentacion;
 
 
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -11,8 +13,11 @@ import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import java.awt.event.ActionListener;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
@@ -26,8 +31,6 @@ public class VentanaBuscar extends JFrame {
 	private static final long serialVersionUID = 3250008474179648164L;
 	private JPanel contentPane;
 	private JTextField folioTF;
-	private JTextField textField_3;
-	private JTextField textField_6;
 	private JTextField nombreTF;
 	private JTextField fnacimientoTF;
 	private JTextField emailTF;
@@ -64,14 +67,58 @@ public class VentanaBuscar extends JFrame {
 	 * Create the frame.
 	 */
 	public VentanaBuscar() {
-		setTitle("Busqueda de Cliente");
+		setTitle("Búsqueda de Cliente");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 527, 356);
+		setBounds(100, 100, 606, 411);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		
-		JButton botonBuscaCliente = new JButton("Buscar");
+	
+		JLabel lblFolio = new JLabel("Folio:");
+		folioTF = new JTextField();
+		folioTF.setColumns(10);
+		folioTF.setEnabled(true);
+		folioTF.setEditable(true);
+		
+		lblNombre = new JLabel("Nombre:");
+		nombreTF = new JTextField();
+		nombreTF.setEditable(true);
+		nombreTF.setEnabled(true);
+		nombreTF.setColumns(10);
+		
+		lblApellidos = new JLabel("Apellidos:");
+		apellidoTF = new JTextField();
+		apellidoTF.setEditable(true);
+		apellidoTF.setEnabled(true);
+		apellidoTF.setColumns(10);
+		
+		lblFechaDeNacimiento = new JLabel("Edad");
+		fnacimientoTF = new JTextField();
+		fnacimientoTF.setEnabled(true);
+		fnacimientoTF.setEditable(true);
+		fnacimientoTF.setColumns(10);
+		
+		lblCorreo = new JLabel("E-mail:");
+		emailTF = new JTextField();
+		emailTF.setEditable(true);
+		emailTF.setEnabled(true);
+		emailTF.setColumns(10);
+		
+		lblDireccion = new JLabel("Dirección:");
+		direccionTF = new JTextField();
+		direccionTF.setEditable(true);
+		direccionTF.setEnabled(true);
+		direccionTF.setColumns(10);
+		
+		lblTelefono = new JLabel("Teléfono:");
+		telefonoTF = new JTextField();
+		telefonoTF.setEditable(true);
+		telefonoTF.setEnabled(true);
+		telefonoTF.setColumns(10);
+		
+	JButton button= new JButton("Historial Clinico");
+	JButton botonBuscaCliente = new JButton("Buscar");
 		
 		botonBuscaCliente.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -85,64 +132,48 @@ public class VentanaBuscar extends JFrame {
 						direccionTF.setText(ConnectDB.getDireccion());
 						emailTF.setText(ConnectDB.getCorreo());
 						telefonoTF.setText(ConnectDB.getTelefono());
+						
+					if(ConnectDB.getNombre()!=null){
+							button.setEnabled(true);	
+						}
+						else{
+							button.setEnabled(false);
+						}
+						
 					} catch (SQLException e) {
 						e.printStackTrace();
 					}
 			}
 		});
 		
-		folioTF = new JTextField();
-		folioTF.setColumns(10);
+			
+		JButton btnEditar = new JButton("Editar");
 		
-		textField_3 = new JTextField();
-		textField_3.setColumns(10);
+		btnEditar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+								
+						try{
+				            PreparedStatement guardar = ConnectDB.Conectar().
+				                    prepareStatement("UPDATE pacientes set nombre='"+nombreTF.getText()+"',apellidos='"+apellidoTF.getText()+
+				                    		"',telefono='"+telefonoTF.getText()+"',correo='"+emailTF.getText()+"',fh_nacimiento='"+Integer.parseInt(fnacimientoTF.getText())+
+				                    		"', direccion='"+direccionTF.getText()+ "'where fol_paciente ="+Integer.parseInt(folioTF.getText()));                                 
+				             
 		
-		textField_6 = new JTextField();
-		textField_6.setColumns(10);
-		
-		JLabel lblFolio = new JLabel("Folio:");
-		
-		nombreTF = new JTextField();
-		nombreTF.setEditable(false);
-		nombreTF.setEnabled(false);
-		nombreTF.setColumns(10);
-		
-		fnacimientoTF = new JTextField();
-		fnacimientoTF.setEnabled(false);
-		fnacimientoTF.setEditable(false);
-		fnacimientoTF.setColumns(10);
-		
-		emailTF = new JTextField();
-		emailTF.setEditable(false);
-		emailTF.setEnabled(false);
-		emailTF.setColumns(10);
-		
-		apellidoTF = new JTextField();
-		apellidoTF.setEditable(false);
-		apellidoTF.setEnabled(false);
-		apellidoTF.setColumns(10);
-		
-		direccionTF = new JTextField();
-		direccionTF.setEditable(false);
-		direccionTF.setEnabled(false);
-		direccionTF.setColumns(10);
-		
-		telefonoTF = new JTextField();
-		telefonoTF.setEditable(false);
-		telefonoTF.setEnabled(false);
-		telefonoTF.setColumns(10);
-		
-		lblNombre = new JLabel("Nombre:");
-		
-		lblApellidos = new JLabel("Apelliods:");
-		
-		lblFechaDeNacimiento = new JLabel("Fecha de nacimiento:");
-		
-		lblDireccion = new JLabel("Direccion:");
-		
-		lblCorreo = new JLabel("E-mail:");
-		
-		lblTelefono = new JLabel("Telefono:");
+				            int update =guardar.executeUpdate();
+				            if(update==1){
+				                JOptionPane.showMessageDialog(null, "Datos guardados con exito");
+				         
+				           
+				            }else {
+				                JOptionPane.showMessageDialog(null, "Datos NO guardados");
+				            }
+				                                 
+				        } catch(SQLException err) {
+				            JOptionPane.showMessageDialog(null, "Hubo un error en la tabla" + err);
+				        }
+					}
+				});
+	
 		
 		JButton botonCancelar= new JButton("Cancelar");
 		
@@ -153,6 +184,32 @@ public class VentanaBuscar extends JFrame {
 		});
 		
 		
+		button.setEnabled(false);
+		button.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent arg0) {
+				
+			HistorialClinico newwindows;
+			
+			try { 
+				newwindows= new HistorialClinico(folioTF.getText());
+				newwindows.setVisible(true);
+				newwindows.setTitle("Historial Clínico");
+				Dimension screen=Toolkit.getDefaultToolkit().getScreenSize();
+				int height = screen.height;
+				int width =screen.width;
+				newwindows.setLocation(50,50);
+				newwindows.setSize(width-100,height-100);
+				newwindows.setResizable(false);
+				
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+	
+			}
+		});
+		
+	
 		
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
@@ -162,94 +219,98 @@ public class VentanaBuscar extends JFrame {
 						.addGroup(gl_contentPane.createSequentialGroup()
 							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 								.addGroup(gl_contentPane.createSequentialGroup()
-									.addGap(29)
-									.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+									.addContainerGap()
+									.addComponent(nombreTF, GroupLayout.PREFERRED_SIZE, 191, GroupLayout.PREFERRED_SIZE))
 								.addGroup(gl_contentPane.createSequentialGroup()
-									.addGap(118)
-									.addComponent(lblFolio))
+									.addContainerGap()
+									.addComponent(fnacimientoTF, GroupLayout.PREFERRED_SIZE, 191, GroupLayout.PREFERRED_SIZE))
 								.addGroup(gl_contentPane.createSequentialGroup()
-									.addGap(48)
-									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-										.addComponent(nombreTF, GroupLayout.PREFERRED_SIZE, 191, GroupLayout.PREFERRED_SIZE)
-										.addComponent(folioTF, GroupLayout.PREFERRED_SIZE, 191, GroupLayout.PREFERRED_SIZE)
-										.addComponent(fnacimientoTF, GroupLayout.PREFERRED_SIZE, 191, GroupLayout.PREFERRED_SIZE)
-										.addComponent(emailTF, GroupLayout.PREFERRED_SIZE, 191, GroupLayout.PREFERRED_SIZE))))
-							.addGap(28)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+									.addContainerGap()
+									.addComponent(emailTF, GroupLayout.PREFERRED_SIZE, 191, GroupLayout.PREFERRED_SIZE))
 								.addGroup(gl_contentPane.createSequentialGroup()
-									.addComponent(textField_6, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-									.addGap(47))
+									.addGap(90)
+									.addComponent(lblFechaDeNacimiento))
 								.addGroup(gl_contentPane.createSequentialGroup()
-									.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-										.addComponent(apellidoTF, GroupLayout.PREFERRED_SIZE, 191, GroupLayout.PREFERRED_SIZE)
-										.addComponent(direccionTF, GroupLayout.PREFERRED_SIZE, 191, GroupLayout.PREFERRED_SIZE)
-										.addComponent(telefonoTF, GroupLayout.PREFERRED_SIZE, 191, GroupLayout.PREFERRED_SIZE))
-									.addGap(49))
+									.addGap(85)
+									.addComponent(lblNombre)))
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 								.addGroup(gl_contentPane.createSequentialGroup()
-									.addComponent(botonBuscaCliente)
 									.addGap(18)
-									.addComponent(botonCancelar)
-									.addGap(43))))
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+										.addGroup(gl_contentPane.createSequentialGroup()
+											.addComponent(direccionTF, GroupLayout.PREFERRED_SIZE, 191, GroupLayout.PREFERRED_SIZE)
+											.addGap(18)
+											.addComponent(button, GroupLayout.PREFERRED_SIZE, 133, GroupLayout.PREFERRED_SIZE))
+										.addGroup(gl_contentPane.createSequentialGroup()
+											.addComponent(apellidoTF, GroupLayout.PREFERRED_SIZE, 191, GroupLayout.PREFERRED_SIZE)
+											.addGap(18)
+											.addComponent(btnEditar, GroupLayout.PREFERRED_SIZE, 133, GroupLayout.PREFERRED_SIZE))
+										.addGroup(gl_contentPane.createSequentialGroup()
+											.addComponent(telefonoTF, GroupLayout.PREFERRED_SIZE, 191, GroupLayout.PREFERRED_SIZE)
+											.addGap(18)
+											.addComponent(botonCancelar, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addGap(87)
+									.addComponent(lblDireccion))
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addGap(81)
+									.addComponent(lblApellidos))))
 						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(112)
-							.addComponent(lblNombre)
-							.addGap(154)
-							.addComponent(lblApellidos))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(72)
-							.addComponent(lblFechaDeNacimiento)
-							.addGap(93)
-							.addComponent(lblDireccion))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(111)
+							.addGap(89)
 							.addComponent(lblCorreo)
-							.addGap(153)
-							.addComponent(lblTelefono)))
-					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+							.addGap(168)
+							.addComponent(lblTelefono))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(folioTF, GroupLayout.PREFERRED_SIZE, 191, GroupLayout.PREFERRED_SIZE)
+							.addGap(30)
+							.addComponent(botonBuscaCliente, GroupLayout.PREFERRED_SIZE, 165, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(81)
+							.addComponent(lblFolio)))
+					.addContainerGap(19, Short.MAX_VALUE))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addContainerGap()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
-							.addComponent(lblFolio)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(folioTF, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE)
-							.addGap(9))
-						.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-								.addComponent(botonBuscaCliente, GroupLayout.DEFAULT_SIZE, 51, Short.MAX_VALUE)
-								.addComponent(botonCancelar, GroupLayout.DEFAULT_SIZE, 48, Short.MAX_VALUE))
-							.addGap(18)))
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(lblNombre)
-						.addComponent(lblApellidos))
+					.addComponent(lblFolio)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(folioTF, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE)
+						.addComponent(botonBuscaCliente, GroupLayout.PREFERRED_SIZE, 39, GroupLayout.PREFERRED_SIZE))
+					.addGap(9)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblApellidos)
+						.addComponent(lblNombre))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
 						.addComponent(nombreTF, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE)
-						.addComponent(apellidoTF, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE))
-					.addGap(12)
+						.addComponent(apellidoTF, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnEditar, GroupLayout.PREFERRED_SIZE, 39, GroupLayout.PREFERRED_SIZE))
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(lblFechaDeNacimiento)
-						.addComponent(lblDireccion))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(12)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+								.addComponent(lblDireccion)
+								.addComponent(lblFechaDeNacimiento))
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+								.addComponent(fnacimientoTF, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE)
+								.addComponent(direccionTF, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE))
+							.addGap(18)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+								.addComponent(lblCorreo)
+								.addComponent(lblTelefono)))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(29)
+							.addComponent(button, GroupLayout.PREFERRED_SIZE, 39, GroupLayout.PREFERRED_SIZE)))
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(fnacimientoTF, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE)
-						.addComponent(direccionTF, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE))
-					.addGap(18)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(lblCorreo)
-						.addComponent(lblTelefono))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(emailTF, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE)
 						.addComponent(telefonoTF, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE)
-						.addComponent(emailTF, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE))
-					.addGap(66)
-					.addComponent(textField_6, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE)
-					.addGap(35))
+						.addComponent(botonCancelar, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(82, Short.MAX_VALUE))
 		);
 		contentPane.setLayout(gl_contentPane);
 	}
